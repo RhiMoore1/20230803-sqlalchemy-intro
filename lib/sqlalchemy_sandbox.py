a466107ddd82c4258e940613baeed6c75d195b61
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-08-12 18:56:56 -0700

    completes lab

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -20,7 +20,7 @@ Base = declarative_base()
 class Student(Base):
     __tablename__ = 'students'
     Index('index_name', 'name')
-
+# 1. ✅ Develop a student schema: name, email, grade, birthday(year,month,day)
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     email = Column(String(55))
@@ -40,12 +40,118 @@ if __name__ == '__main__':
     session = Session()
 
 
+# 3. ✅ Query all students. 
+students = session.query(Student)
+print([student for student in students])
+
+students = session.query(Student).all()
+print(students)
+
+
+# 4. ✅ Query all students by name. 
+students = session.query(Student.name).all()
+print(students)
+
+# 5. ✅ Query all students by name, and order by name. 
+students = session.query(Student.name).order_by(Student.name).all()
+print(students)
+
+# 6. ✅ Query all students by name, order by name, descending.
+students = session.query(Student.name).order_by(desc(Student.name)).all()
+print(students)
+
+# 7. ✅ Limit student query to 1 result
+oldest_student = session.query(Student.name, Student.birthday).order_by(desc(Student.birthday)).limit(2).all()
+print(oldest_student)
+
+# 8. ✅ Use the SQLAlchemy func.count function to report how many student entries exist.
+student_count = session.query(func.count(Student.id)).first()
+print(student_count)
+
+# 9. ✅ Filter student query results by name
+query = session.query(Student).filter(Student.name.like('%Thompson%'), Student.grade ==7).all()
+print(query)
+
+# 10. ✅ Update the student data for a single column.
+for student in session.query(Student):
+    student.grade += 1
+
+    session.commit()
+
+    print([(student.name, student.grade) for student in session.query(Student)])
+
+# 10. ✅ Update the student data for a single column.
+session.query(Student).update({Student.grade: Student.grade+1})
+
+print([(
+    student.name,
+    student.grade
+) for student in session.query(Student)])
+
+# 11. ✅ Delete a student database entry/row. 
 query = session.query(Student).filter(Student.name.like("%Albert%"))
 albert_einstein = query.first()
+print(albert_einstein)
 
 session.delete(albert_einstein)
 session.commit()
 
 albert_einstein = query.first()
 print("Hopefully is None: ", albert_einstein)
+
+# 2. ✅ Create a new student record and add it to the database
+# albert_einstein = Student (
+#     name="Albert Einstein",
+#     email="albert.einstein@flatironschool.com",
+#     grade=4,
+#     birthday = datetime(
+#         year=1945,
+#         month=11,
+#         day=11
+#     )
+# )
+
+
+# 2. ✅ Create a new student record and add it to the database
+# /////////////////////////////////////////////////////////
+# joe_stee = Student (
+#     name="Joe Stee",
+#     email="joe.stee@flatironschool.com",
+#     grade=4,
+#     birthday = datetime(
+#         year=1995,
+#         month=2,
+#         day=12
+#     )
+# )
+
+# crab_shack = Student (
+#     name="Crab Shack",
+#     email="crab_shack@flatironschool.com",
+#     grade=4,
+#     birthday = datetime(
+#         year=1975,
+#         month=9,
+#         day=3
+#     )
+# )
+
+# session.bulk_save_objects([joe_stee, crab_shack])
+# session.commit()
+# print(f"New student ID is {joe_stee.id}")
+# print(f"New student ID is {crab_shack.id}")
+# ///////////////////////////////////////////////////////////////
+
+
+
+
+
+# query = session.query(Student).filter(Student.name.like("%Albert%"))
+# albert_einstein = query.first()
+
+# session.delete(albert_einstein)
+# session.commit()
+
+# albert_einstein = query.first()
+# print("Hopefully is None: ", albert_einstein)
 
